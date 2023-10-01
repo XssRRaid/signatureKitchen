f04e3d6b41395fe97d37dd63124417af6de4f296
Sarath <sarath.unnithan@gmail.com>
2023-10-02 00:25:22 +0530

    Updates

---
+++ _nuxt/test.21189e6d.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./Testimonial.41fb3536.js";import{_ as e}from"./_plugin-vue_export-helper.c27b6911.js";import{y as n,z as r,x as c}from"./swiper-vue.e5334e62.js";import"./entry.f43e4c42.js";const _={};function s(a,m){const o=t;return c(),n("div",null,[r(o)])}const u=e(_,[["render",s]]);export{u as default};
